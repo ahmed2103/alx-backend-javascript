a1fb4c6ecda2e73edbe4a8ad85a3bb574bdada09
Hasona <hasona@kali.kali>
2024-06-27 01:49:48 +0300

    promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getResponseFromAPI(success) {
+    return new promise((resolve, reject) => {
+        if (success) {
+            resolve({
+                status: 200,
+                body: 'Success',
+            });
+        } else {
+                reject(new Error('The fake API is not working currently'));
+            }
+    });
+}
---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,17 @@
+export default handleResponseFromAPI(promise)
+{
+    return promise
+        .then(() => {
+            return {
+                status: 200,
+                body: 'success',
+            };
+        })
+        .catch(() => {
+            return new Error('The fake API is not working currently');
+        });
+.
+    finally(() => {
+        console.log('Got a response from the API');
+    });
+}
