fa831fb4d91b923b6a4df32268dc2635c30ccb26
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-06-28 17:41:55 +0300

    Update 6-string-interpolation.js

--- 0x00-ES6_basic/6-string-interpolation.js
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -1,10 +1,12 @@
 export default function getSanFranciscoDescription() {
-    const year = 2017;
-    const budget = {
-        income: '$119,868',
-        gdp: '$154.2 billion',
-        capita: '$178,479',
-    };
+  const year = 2017;
+  const budget = {
+    income: '$119,868',
+    gdp: '$154.2 billion',
+    capita: '$178,479',
+  };
 
-    return `As of ${year}, San Francisco's income was ${budget.income}, GDP was ${budget.gdp}, and per capita was ${budget.capita}.`;
+  return `As of ${year}, it was the seventh-highest income county in the United States\
+, with a per capita personal income of ${budget.income}. As of 2015, San Francisco \
+proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
