23b6009f8fb12332ade85816b9a0072d007ebf88
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-07-02 22:31:51 +0300

    Update 9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,10 @@
-export default function groceryList() {
-    return new Map([
-        ["Apples", 10],
-        ["Tomatoes", 10],
-        ["Pasta", 1],
-        ["Rice", 1],
-        ["Banana", 5],
-    ]);
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
 }
