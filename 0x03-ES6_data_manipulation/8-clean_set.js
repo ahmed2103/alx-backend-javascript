ac85afafd798a6d54fb1f671ce381465a888fdb6
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-07-02 22:30:58 +0300

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,16 @@
 export default function cleanSet(set, startString) {
-    const arr = [];
-    for (const value of set) {
-       if (value.startsWith(startString)) {
-           arr.push(value.slice(startString.length));
-       }
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
     }
-    return arr.join('-');
+  }
+  return parts.join('-');
 }
